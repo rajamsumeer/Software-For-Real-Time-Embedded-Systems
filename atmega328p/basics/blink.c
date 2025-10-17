2203df12d8bede908cf887c7ed79c73aa29d5d69
Raja Sumeer <rajamsumeer@gmail.com>
2025-10-18 00:30:43 +0100

    C:/Program Files/Git/

--- atmega328p/basics/blink.c
+++ atmega328p/basics/blink.c
@@ -1,16 +1,17 @@
-#include <avr/io.h> 
+#include <avr/io.h> // AVR input/output definitions
 
-#define F_CPU 1000000UL 
+#define F_CPU 1000000UL // CPU clock speed @1MHz (util/delay)
 
-#include <util/delay.h> 
+#include <util/delay.h> // library for _delay_ms()
 
 int main(void) 
 { 
-  DDRB |= (1<<5); 
+  DDRB |= (1<<5); // set PB5 as output
 
-  while(1)  { 
-  PORTB ^= (1<<5); 
+  while(1) // infinite loop
+  { 
+  PORTB ^= (1<<5); // toggle (OR operation) PB5 (LED)
   _delay_ms(500); 
   } 
    return 0; 
- }                      
+}
